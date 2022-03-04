195ff520606e18f94790e7377ee349fc96eb86ab
Jaden Wang <Jaden.Shiteng.Wang@gmail.com>
2022-03-04 10:22:39 -0800

    added sources for A2

---
+++ source/summary.R
@@ -0,0 +1,37 @@
+library(readr)
+cpi <- read_csv("data/inflation.csv")
+
+library(dplyr)
+library(stringi)
+mean_cpi <- cpi %>% 
+  mutate(Year = stri_sub(Yearmon, -4)) %>%
+  group_by(Year) %>% 
+  summarize(CPIYearMean = mean(CPI)) %>% 
+  filter(between(Year, 1960, 2015))
+
+# highest_cpi <- mean_cpi %>% arrange(desc(CPIYearMean))
+
+fed_rate <- read_csv("data/federal_interest_rates.csv") %>% 
+  filter(between(Year, 1960, 2015))
+fed_rate_mean <- fed_rate %>% 
+  group_by(Year) %>% 
+  summarize(RateYearMean = mean(`Effective Federal Funds Rate`, na.rm=TRUE))
+
+inflation_mean <- fed_rate %>% 
+  group_by(Year) %>% 
+  summarize(InflationMean = mean(`Inflation Rate`, na.rm=TRUE))
+
+unemployment_mean <- fed_rate %>% 
+  group_by(Year) %>% 
+  summarize(UnemploymentMean = mean(`Unemployment Rate`, na.rm=TRUE)) 
+
+max_inflation_year <- (inflation_mean %>% 
+  arrange(desc(InflationMean)))[1,]$Year
+
+max_fed_rate_year <- (fed_rate_mean %>% 
+  arrange(desc(RateYearMean)))[1,]$Year
+
+max_unemployment_year <- (unemployment_mean %>% 
+  arrange(desc(UnemploymentMean)))[1,]$Year
+
+
